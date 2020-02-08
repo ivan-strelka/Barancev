40c4d2e4d8ffc6f7e19f55da5c44f459d49b17be
ivan-strelka <ivan.strelka2@gmail.com>
2020-02-08 18:38:01 +0300

    first commit

---
+++ Sandbox/src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point A = new Point(2, 4);
+        Point B = new Point(5, 7);
+        System.out.println("Посчитаем расстояние между точками " + A.x +
+                " и " + A.y + " + " + B.x + " и " + B.y + " оно равно " + +distance(A, B));
+
+
+        System.out.println("Посчитаем расстояние между точками вторым способом " + A.x +
+                " и " + A.y + " + " + B.x + " и " + B.y + " оно равно " +A.distance(B));
+    }
+
+    public static double distance(Point point, Point point1) {
+        double f = Math.sqrt((point1.x - point.x) * (point1.x - point.x) + (point1.y - point.y) * (point1.y - point.y));
+        return f;
+
+    }
+
+
+}
---
+++ Sandbox/src/main/java/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance( Point p2){
+        double dx = p2.x-this.x;
+        double dy = p2.y-this.y;
+        return Math.sqrt(dx*dx + dy*dy);
+
+    }
+
+
+}
+
+
