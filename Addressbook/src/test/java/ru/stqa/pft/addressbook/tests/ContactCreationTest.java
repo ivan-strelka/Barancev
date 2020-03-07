31c653c062e6189a7add04df8dd992edfcce8aff
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-07 13:49:48 +0300

    step 9

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class ContactCreationTest extends TestBase {
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().goToContactPage();
         ContactData contactData = new ContactData("Fuller",
-                "Brad",
+                "Brad2",
                 "lazinywiqa@mailinator.com",
                 "Consectetur lorem re",
                 "aaa");
@@ -24,13 +25,7 @@ public class ContactCreationTest extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contactData.setId(max);
+        contactData.setId(after.stream().max(Comparator.comparingInt(o -> o.getId())).get().getId());
         before.add(contactData);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
