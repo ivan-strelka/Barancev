362468c50091db8ff98dd49cac5e7c1b39ba6b3e
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-16 15:37:06 +0300

    step 5.11

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -3,18 +3,34 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTest extends TestBase {
+
+    public static String cleaned(String phone) {
+        return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+    }
 
     @Test
     public void testContactPhone() {
         app.goTo().goToHomePage();
         ContactData contact = app.goToCont().all().iterator().next();
         ContactData contactInfoFromEditForm = app.goToCont().infoFromEditForm(contact);
-        assertThat(contact.getHomePhone(), equalTo(contactInfoFromEditForm.getHomePhone()));
-        assertThat(contact.getWorkPhone(), equalTo(contactInfoFromEditForm.getWorkPhone()));
+
+        assertThat(contact.getAllPhone(), equalTo(mergePhones(contactInfoFromEditForm)));
+
+    }
+
+    private String mergePhones(ContactData contact) {
+        return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+                .stream().filter((s) -> !s.equals(""))
+                .map(ContactPhoneTest::cleaned)
+                .collect(Collectors.joining("\n"));
+
 
     }
 
