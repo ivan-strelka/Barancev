3d3cf9b4aaf2063538f1d576b931fd35227a56d1
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-18 22:26:22 +0300

    step 6.7

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -19,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsFromXML() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
         String xml = "";
         String line = reader.readLine();
@@ -36,8 +38,26 @@ public class GroupCreationTest extends TestBase {
 
     }
 
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJSON() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+        }.getType());
+        return groups.stream().map((g) -> new Object[]{g})
+                .collect(Collectors.toList())
+                .iterator();
 
-    @Test(dataProvider = "validGroups")
+    }
+
+
+    @Test(dataProvider = "validGroupsFromJSON")
     public void testGroupCreation(GroupData group) {
         app.goTo().GroupPage();
         Groups before = app.group().all();
