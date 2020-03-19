ee0e6058fa880173da5baf25183f204cb85eec32
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-19 10:21:12 +0300

    step 6.4 contacts

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,27 +1,36 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test
-    public void testCreationContact() {
+    @DataProvider
+    public Iterator<Object[]> validContact() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new ContactData().withFirstName("firstName 1").withLastName("lastName 1").withGroup("aaa")});
+        list.add(new Object[]{new ContactData().withFirstName("firstName 2").withLastName("lastName 2").withGroup("aaa")});
+        list.add(new Object[]{new ContactData().withFirstName("firstName 3").withLastName("lastName 3").withGroup("aaa")});
+        return list.iterator();
+    }
+
+
+    @Test(dataProvider = "validContact")
+    public void testCreationContact(ContactData contact) {
         Contacts before = app.goToCont().all();
         app.goTo().ContactPage();
         File photo = new File("src/test/resources/testPhoto.png");
-        ContactData contact = new ContactData().withFirstName("Fuller")
-                .withLastName("Brad2")
-                .withEmail("lazinywiqa@mailinator.com")
-                .withAddress("Consectetur lorem re")
-                .withPhoto(photo)
-                .withGroup("aaa");
         app.goToCont().create(contact, true);
         app.goTo().goToHomePage();
         assertThat(app.goToCont().Count(), equalTo(before.size() + 1));
