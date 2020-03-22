28ca9c7d37417087f262019737d801ae7e0efd04
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-22 10:16:21 +0300

    logging

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -20,6 +20,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
+
     @DataProvider
     public Iterator<Object[]> validGroupsFromXML() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"))) {
--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.File;
 import java.io.FileReader;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static final ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     public Properties properties;
@@ -25,7 +34,18 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
-
     }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("START TEST " + m.getName() + "with parameters" + Arrays.asList(p));
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("STOP TEST " + m.getName());
+    }
+
 
 }
