0080d9acdf0658ca6b564add3de34cea386d8dba
ivan-strelka <ivan.strelka2@gmail.com>
2020-03-22 21:32:49 +0300

    7.3 step

--- Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ Addressbook/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
@@ -43,4 +44,18 @@ public class HbConnectionTest {
         session.close();
 
     }
+
+    @Test
+    public void testHbConnectionContact() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery("from ContactData").list();
+        for (ContactData contacts : result) {
+            System.out.println(contacts);
+        }
+        System.out.println("SIZE IS " + result.size());
+        session.getTransaction().commit();
+        session.close();
+    }
+
 }
